8738d3c5a82039708795de6d48cf7540104ed88a
Arturo Guaman <arturoguaman1047@hotmail.com>
2025-06-03 17:03:18 -0400

    update promptGen added buildspec

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+//array of prompts 
 const prompts = [
   "Write a story set on Mars.",
   "Describe a world where gravity is reversed.",
@@ -15,18 +16,43 @@ const prompts = [
 ];
 
 function App() {
-  const [prompt, setPrompt] = useState("Click the button to get a random prompt!");
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [prompt, setPrompt] = useState('');
+  const [running, setRunning] = useState(false);
 
+  //countdown timer
+  useEffect(() => {
+    let timer;
+    if (running && timeLeft > 0) { //time greater than 0 continue count down
+      timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000); //every second subtract one from timer 
+    } else if (running && timeLeft === 0) { //time reaches 0
+      setPrompt(''); //set prompt to blank
+      setRunning(false); 
+    }
+    return () => clearTimeout(timer); //clean up timer
+  }, [timeLeft, running]);
+
+  // generate random prompt
   const generatePrompt = () => {
     const randomIndex = Math.floor(Math.random() * prompts.length);
     setPrompt(prompts[randomIndex]);
   };
 
+  //start timer function
+  const startTimer = () => {
+    setTimeLeft(10);
+    generatePrompt();
+    setRunning(true);
+  };
+
   return (
     <div className="App">
+      <div className="timer">Time left: {timeLeft}s</div>
       <h1>Vibe Match</h1>
       <div className="prompt-box">{prompt}</div>
-      <button onClick={generatePrompt}>Generate Prompt</button>
+       <button onClick={startTimer}>
+        {running ? "Running..." : "Start Round"}
+      </button>
       <div className="spotify">
         <iframe 
           src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M" 
